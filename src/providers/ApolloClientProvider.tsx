f816c2db08ad3f251bb455e86c118b952747f6a8
Nevil Jobanputra <nevil.j@ahduni.edu.in>
2023-10-21 17:03:22 +0530

    Recommendation

---
+++ src/providers/ApolloClientProvider.tsx
@@ -0,0 +1,16 @@
+import { ApolloClient, InMemoryCache, ApolloProvider} from "@apollo/client";
+import { PropsWithChildren } from "react";
+
+const client = new ApolloClient({
+    uri: 'https://azcoitia.stepzen.net/api/handy-wolf/__graphql',
+    headers: {
+        Authorization: 'apikey azcoitia::stepzen.net+1000::fb8e31b5fb9a7521aca53ce3d8ef5b43f2392865a30c1098cb4db5962713b133',
+    },
+    cache: new InMemoryCache(),
+});
+
+const ApolloClientProvider = ({ children }: PropsWithChildren) => {
+    return <ApolloProvider client={client}>{children}</ApolloProvider>;
+}
+
+export default ApolloClientProvider;
